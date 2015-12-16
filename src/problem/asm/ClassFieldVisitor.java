52c58902920eb03c778a63ac8b37b65147cc26ee
Jack Porter <porterjc@rose-hulman.edu>
2015-12-16 17:02:53 -0500

    Fields are now being added to the ClassData. FieldData has a contructor and appropiate get-ers now.

--- src/problem/asm/ClassFieldVisitor.java
+++ src/problem/asm/ClassFieldVisitor.java
@@ -2,16 +2,28 @@ package problem.asm;
 
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 
 public class ClassFieldVisitor extends ClassVisitor {
+	private String level = "";
+	private ClassData data;
+	
 	public ClassFieldVisitor(int api) {
 		super(api);
+		this.data = new ClassData();
 	}
 
 	public ClassFieldVisitor(int api, ClassVisitor decorated) {
 		super(api, decorated);
+		this.data = new ClassData();
 	}
+	
+	public ClassFieldVisitor(int api, ClassDeclarationVisitor decorated) {
+		super(api, decorated);
+		this.data = decorated.getClassData();
+	}
+
 
 	public FieldVisitor visitField(int access, String name, String desc,
 			String signature, Object value) {
@@ -20,8 +32,30 @@ public class ClassFieldVisitor extends ClassVisitor {
 		String type = Type.getType(desc).getClassName();
 		// TODO: delete the line below
 		System.out.println(" " + type + " " + name);
+		
+		addAccessLevel(access);
+		
+		data.addField(new FieldData(name, level, type));
+		//appendToField(name + " : " + type + "'\'l");
 		// TODO: add this field to your internal representation of the current class.
 		// What is a good way to know what the current class is?
 		return toDecorate;
 	};
+	
+	public void addAccessLevel(int access) {
+		if ((access & Opcodes.ACC_PUBLIC) != 0) {
+			level = "+";
+		} else if ((access & Opcodes.ACC_PROTECTED) != 0) {
+			level = "protected";
+		} else if ((access & Opcodes.ACC_PRIVATE) != 0) {
+			level = "-";
+		} else {
+			level = "+";
+		}
+		// TODO: ADD this information to your representation of the current method.
+	}
+	
+	public ClassData getClassData() {
+		return data;
+	}
 }
--- src/problem/asm/FieldData.java
+++ src/problem/asm/FieldData.java
@@ -1,5 +1,25 @@
 package problem.asm;
 
 public class FieldData {
-	//private String
+	private String fieldName;
+	private String access;
+	private String type;
+	
+	public FieldData(String name, String access, String type) {
+		this.fieldName = name;
+		this.access = access;
+		this.type = type;
+	}
+	
+	public String getFieldName() {
+		return fieldName;
+	}
+	
+	public String getAccessLevel() {
+		return access;
+	}
+	
+	public String getType() {
+		return type;
+	}
 }

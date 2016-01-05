9adaa1a68cb06c7201387a7c9a8d21ac7982aa51
Jack Porter <porterjc@rose-hulman.edu>
2016-01-04 22:52:11 -0500

    Added test driver class, as well as a .pdf and .docx of the UML design for lab 1-3 and the Design Parser.

---
+++ test/problem/asm/AllTests.java
@@ -0,0 +1,13 @@
+package problem;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	ClassDataTest.class
+})
+public class AllTests {
+	
+}
--- test/problem/asm/ClassDataTest.java
+++ test/problem/asm/ClassDataTest.java
@@ -17,16 +17,11 @@ public class ClassDataTest {
 	@Test
 	public final void testSingleClass() throws IOException, InterruptedException {
 		ClassReader reader = new ClassReader("problem.asm.FirstASM");
-		// make class declaration visitor to get superclass and interfaces
 		AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, null);
-		// DECORATE declaration visitor with field visitor
 		AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5,
 				decVisitor);
-		// DECORATE field visitor with method visitor
 		AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5,
 				fieldVisitor);
-		// TODO: add more DECORATORS here in later milestones to accomplish specific tasks
-		// Tell the Reader to use our (heavily decorated) ClassVisitor to visit the class
 		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
 		assertEquals("FirstASM [\n" + "label = " +
 		"{FirstASM|+ myField : java.lang.String\l|+ clinit() : void\l+ init() :"

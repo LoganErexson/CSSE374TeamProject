9a2564f99960162ce1e4697f0351ee520e6ade8d
Jack Porter <porterjc@rose-hulman.edu>
2016-01-06 17:55:48 -0500

    Added new test for associations

--- test/problem/asm/ClassDataTest.java
+++ test/problem/asm/ClassDataTest.java
@@ -14,59 +14,69 @@ import org.objectweb.asm.Type;
 public class ClassDataTest {
 	@Test
 	public final void testSingleClass() throws IOException {
-		ClassReader reader = new ClassReader("problem.asm.FirstASM");
-		AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, null);
-		AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5,
-				decVisitor);
-		AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5,
-				fieldVisitor);
+		ClassReader reader = new ClassReader("problem.asm.GraphVisPrinter");
+		AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(
+				Opcodes.ASM5, null);
+		AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(
+				Opcodes.ASM5, decVisitor);
+		AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(
+				Opcodes.ASM5, fieldVisitor);
 		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
-		assertEquals("FirstASM [\n" + "label = " + "\"{FirstASM|+ myField : java.lang.String\\l|+ clinit() : void\\l+ init() :"
-		+ " void\\l+ main(java.lang.String[]) : void\\l}\"\n" + "]\n", methodVisitor.getClassData().toString());
+		assertEquals(
+				"GraphVisPrinter [\n"
+						+ "label = \"{GraphVisPrinter||+ init() : void\\l+ makeUML(java.lang.String, java.util.List) : void\\l+ getClassNames(java.util.List) : java.util.List\\l}\"\n"
+						+ "]\n", methodVisitor.getClassData().toString());
 	}
-	
+
 	@Test
 	public final void testInterfaces() throws IOException {
 		ClassReader reader = new ClassReader("lab1_3/HTMLBehavior");
-		AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, null);
-		AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5,
-				decVisitor);
-		AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5,
-				fieldVisitor);
+		AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(
+				Opcodes.ASM5, null);
+		AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(
+				Opcodes.ASM5, decVisitor);
+		AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(
+				Opcodes.ASM5, fieldVisitor);
 		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
 		assertEquals("edge [ \n" + "arrowhead = \"empty\"\n"
-				+ "style = \"dashed\"\n]\nHTMLBehavior -> Behavior\n", methodVisitor.getClassData().getInheritsArrows());
+				+ "style = \"dashed\"\n]\nHTMLBehavior -> Behavior\n",
+				methodVisitor.getClassData().getInheritsArrows());
 	}
-	
+
 	@Test
 	public final void testSuperClasses() throws IOException {
-		String[] CLASSES = {"problem.asm.ClassData", "problem.asm.ClassDeclarationVisitor", "problem.asm.ClassFieldVisitor", 
-				"problem.asm.DesignParser", "problem.asm.FieldData", "problem.asm.MethodData", "problem.asm.AbstractClassDataVisitor"};
+		String[] CLASSES = { "problem.asm.ClassData",
+				"problem.asm.ClassDeclarationVisitor",
+				"problem.asm.ClassFieldVisitor", "problem.asm.DesignParser",
+				"problem.asm.FieldData", "problem.asm.MethodData",
+				"problem.asm.AbstractClassDataVisitor" };
 		List<ClassData> classDatas = new ArrayList<>();
 		for (String className : CLASSES) {
 			ClassReader reader = new ClassReader(className);
-			AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, null);
-			AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5,
-					decVisitor);
-			AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5,
-					fieldVisitor);
+			AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(
+					Opcodes.ASM5, null);
+			AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(
+					Opcodes.ASM5, decVisitor);
+			AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(
+					Opcodes.ASM5, fieldVisitor);
 			reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
 			classDatas.add(methodVisitor.getClassData());
 		}
 		ClassReader reader = new ClassReader("problem.asm.ClassMethodVisitor");
-		AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, null);
-		AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5,
-				decVisitor);
-		AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5,
-				fieldVisitor);
+		AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(
+				Opcodes.ASM5, null);
+		AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(
+				Opcodes.ASM5, decVisitor);
+		AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(
+				Opcodes.ASM5, fieldVisitor);
 		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
 		classDatas.add(methodVisitor.getClassData());
 		List<String> classNames = GraphVisPrinter.getClassNames(classDatas);
 		assertEquals("edge [ \n" + "arrowhead = \"empty\"\n]\n"
-				+ "ClassMethodVisitor -> AbstractClassDataVisitor\n", 
+				+ "ClassMethodVisitor -> AbstractClassDataVisitor\n",
 				methodVisitor.getClassData().getExtendsArrow(classNames));
 	}
-	
+
 	@Test
 	public final void testToStringBase() {
 		ClassData c = new ClassData();
@@ -79,8 +89,78 @@ public class ClassDataTest {
 		c.addField(new FieldData("AField", "+", f, null));
 		Type[] t = {};
 		c.addMethod(new MethodData("AMEthod", Type.VOID_TYPE, "+", t, ""));
-		
-		assertEquals("Test [\n" + "label = " + "\"{Test|+ AField : C\\l|+ AMEthod() : void\\l"
-				+ "}\"\n" + "]\n", c.toString());
+
+		assertEquals("Test [\n" + "label = "
+				+ "\"{Test|+ AField : C\\l|+ AMEthod() : void\\l" + "}\"\n"
+				+ "]\n", c.toString());
+	}
+
+	@Test
+	public final void testUsesArrow() throws IOException {
+		String[] CLASSES = { "problem.asm.ClassMethodVisitor",
+				"problem.asm.ClassDeclarationVisitor",
+				"problem.asm.ClassFieldVisitor", "problem.asm.DesignParser",
+				"problem.asm.FieldData", "problem.asm.MethodData",
+				"problem.asm.AbstractClassDataVisitor" };
+		List<ClassData> classDatas = new ArrayList<>();
+		for (String className : CLASSES) {
+			ClassReader reader = new ClassReader(className);
+			AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(
+					Opcodes.ASM5, null);
+			AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(
+					Opcodes.ASM5, decVisitor);
+			AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(
+					Opcodes.ASM5, fieldVisitor);
+			reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+			classDatas.add(methodVisitor.getClassData());
+		}
+		ClassReader reader = new ClassReader("problem.asm.ClassData");
+		AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(
+				Opcodes.ASM5, null);
+		AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(
+				Opcodes.ASM5, decVisitor);
+		AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(
+				Opcodes.ASM5, fieldVisitor);
+		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+		classDatas.add(methodVisitor.getClassData());
+		List<String> classNames = GraphVisPrinter.getClassNames(classDatas);
+		assertEquals("edge [ \n" + "arrowhead = \"vee\"\n"
+				+ "style = \"dashed\"\n]\n" + "ClassData -> FieldData\n"
+				+ "ClassData -> MethodData\n", methodVisitor.getClassData()
+				.getUsesArrows(classNames));
+	}
+
+	@Test
+	public final void testAssociationArrows() throws IOException {
+		String[] CLASSES = { "problem.asm.ClassMethodVisitor",
+				"problem.asm.ClassDeclarationVisitor",
+				"problem.asm.ClassFieldVisitor", "problem.asm.DesignParser",
+				"problem.asm.ClassData", "problem.asm.MethodData",
+				"problem.asm.AbstractClassDataVisitor" };
+		List<ClassData> classDatas = new ArrayList<>();
+		for (String className : CLASSES) {
+			ClassReader reader = new ClassReader(className);
+			AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(
+					Opcodes.ASM5, null);
+			AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(
+					Opcodes.ASM5, decVisitor);
+			AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(
+					Opcodes.ASM5, fieldVisitor);
+			reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+			classDatas.add(methodVisitor.getClassData());
+		}
+		ClassReader reader = new ClassReader("problem.asm.FieldData");
+		AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(
+				Opcodes.ASM5, null);
+		AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(
+				Opcodes.ASM5, decVisitor);
+		AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(
+				Opcodes.ASM5, fieldVisitor);
+		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+		classDatas.add(methodVisitor.getClassData());
+		List<String> classNames = GraphVisPrinter.getClassNames(classDatas);
+		assertEquals("edge [ \n" + "arrowhead = \"vee\"\n"
+				+"]\n" + "FieldData -> ClassData\n",
+				methodVisitor.getClassData().getAssociationArrows(classNames));
 	}
 }

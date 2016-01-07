d3dae2fbba2cf960d48ecdbec256418459e364b6
Logan Erexson <erexsolr@rose-hulman.edu>
2016-01-06 23:01:10 -0500

    Added Equivalent to pizza code from book

---
+++ src/headfirst/factory/pizzaaf/ChicagoPizzaIngredientFactory.java
@@ -0,0 +1,22 @@
+package headfirst.factory.pizzaaf;
+
+public class ChicagoPizzaIngredientFactory 
+	implements PizzaIngredientFactory 
+{
+
+	public Dough createDough() {
+		return new ThickCrustDough();
+	}
+
+	public Sauce createSauce() {
+		return new PlumTomatoSauce();
+	}
+
+	public Cheese createCheese() {
+		return new MozzarellaCheese();
+	}
+
+	public Clams createClam() {
+		return new FrozenClams();
+	}
+}
---
+++ src/headfirst/factory/pizzaaf/PizzaIngredientFactory.java
@@ -0,0 +1,10 @@
+package headfirst.factory.pizzaaf;
+
+public interface PizzaIngredientFactory {
+ 
+	public Dough createDough();
+	public Sauce createSauce();
+	public Cheese createCheese();
+	public Clams createClam();
+ 
+}

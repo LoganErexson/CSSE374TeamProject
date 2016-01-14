e69203c8d20c1749e20e758ec8194715f40a3638
Logan Erexson <erexsolr@rose-hulman.edu>
2016-01-13 21:25:41 -0500

    Added command line functionality in Designparser
    Format may need changed

--- src/problem/asm/DesignParser.java
+++ src/problem/asm/DesignParser.java
@@ -17,9 +17,10 @@ public class DesignParser {
 	 * For lab 1-3 files: FOLDER_PATH = "./src/lab1_3"; FILE_PREFIX = "lab1_3"
 	 * 
 	 */
-	public static final String FOLDER_PATH = "./src/headfirst/factory/pizzaaf";
-	public static final String FILE_PREFIX = "headfirst.factory.pizzaaf";
-	public static final String OUTPUT_FILE = "./input_output/Diagram.gv";
+	public static final String DEFAULT_PATH = "./src/headfirst/factory/pizzaaf";
+	public static final String DEFAULT_PREFIX = "headfirst.factory.pizzaaf";
+	public static final String UML_OUTPUT = "./input_output/Diagram.gv";
+	public static final String SD_OUTPUT = "./input_output/sDiagram.sd";
 	
 	/**
 	 * Reads in a list of Java Classes and reverse engineers their design.
@@ -27,46 +28,79 @@ public class DesignParser {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		String methPath = "";
-		int depth = 5;
-		if (args.length == 1) {
-			methPath = args[0];
-		} else if (args.length == 2) {
-			methPath = args[0];
-			depth = Integer.parseInt(args[1]);
+		if(args.length<1){
+			System.out.println("NO ARGUMENTS");
+			System.exit(0);
 		}
-		
-		List<String> classes = new ArrayList<>();
-		
-		File classesFolder = new File(FOLDER_PATH);
-		File[] listOfFiles = classesFolder.listFiles();
-		
-		String classPath;
-	    for (int i = 0; i < listOfFiles.length; i++) {
-	      if (listOfFiles[i].isFile()) {
-	    	  classPath = listOfFiles[i].toString();
-	        classes.add(FILE_PREFIX+"."+classPath.substring(classPath.lastIndexOf("\\")+1, classPath.length()-5));
-	      } 
-	    }
-	    
-		List<IClassData> classDatas = new ArrayList<>();
-		for (String className : classes) {
-			// ASM's ClassReader does the heavy lifting of parsing the compiled Java class
-			ClassReader reader = new ClassReader(className);
-			// make class declaration visitor to get superclass and interfaces
-			AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, null);
-			// DECORATE declaration visitor with field visitor
-			AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5,
-					decVisitor);
-			// DECORATE field visitor with method visitor
-			AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5,
-					fieldVisitor);
-			// TODO: add more DECORATORS here in later milestones to accomplish specific tasks
-			// Tell the Reader to use our (heavily decorated) ClassVisitor to visit the class
-			reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
-			classDatas.add(methodVisitor.getClassData());
+		if(args[0].toLowerCase().equals("uml")){
+			if(args.length<3){
+				System.out.println("NOT ENOUGH ARGUMENTS");
+				System.exit(0);
+			}
+			String folderPath = args[1];
+			String filePrefix = args[2];
+			List<String> classes = new ArrayList<>();
+			
+			File classesFolder = new File(folderPath);
+			File[] listOfFiles = classesFolder.listFiles();
+			
+			String classPath;
+		    for (int i = 0; i < listOfFiles.length; i++) {
+		      if (listOfFiles[i].isFile()) {
+		    	  classPath = listOfFiles[i].toString();
+		        classes.add(filePrefix+"."+classPath.substring(classPath.lastIndexOf("\\")+1, classPath.length()-5));
+		      } 
+		    }
+		    
+			List<IClassData> classDatas = new ArrayList<>();
+			for (String className : classes) {
+				// ASM's ClassReader does the heavy lifting of parsing the compiled Java class
+				ClassReader reader = new ClassReader(className);
+				// make class declaration visitor to get superclass and interfaces
+				AbstractClassDataVisitor decVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, null);
+				// DECORATE declaration visitor with field visitor
+				AbstractClassDataVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5,
+						decVisitor);
+				// DECORATE field visitor with method visitor
+				AbstractClassDataVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5,
+						fieldVisitor);
+				// TODO: add more DECORATORS here in later milestones to accomplish specific tasks
+				// Tell the Reader to use our (heavily decorated) ClassVisitor to visit the class
+				reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+				classDatas.add(methodVisitor.getClassData());
+			}
+			IClassStructurePrinter gPrinter = new GraphVisPrinter(classDatas);
+			gPrinter.printToFile(UML_OUTPUT);
 		}
-		IClassStructurePrinter gPrinter = new GraphVisPrinter(classDatas);
-		gPrinter.printToFile(OUTPUT_FILE);
+		else if(args[0].toLowerCase().equals("sd")){
+			if(args.length<3){
+				System.out.println("NO ARGUMENTS");
+				System.exit(0);
+			}
+			int depth = 5;
+			String className;
+			String methodName;
+			int firstParam = -1;
+			if(args[1].contains("depth=")){
+				depth = Integer.parseInt(args[1].replace("depth=", ""));
+				className = args[2];
+				methodName = args[3];
+				firstParam = 4;
+			}
+			else{
+				className = args[1];
+				methodName = args[2];
+				firstParam = 3;
+			}
+			List<String> parameters = new ArrayList<>();
+			int i = firstParam;
+			while(i<args.length){
+				parameters.add(args[i]);
+			}
+		}
+		else{
+			System.out.println("INVALID COMMAND");
+			System.exit(0);
+		}
 	}
 }

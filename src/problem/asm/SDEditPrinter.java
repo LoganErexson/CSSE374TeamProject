c765bc64917c8023161a150f214d9eb649cc006d
Logan Erexson <erexsolr@rose-hulman.edu>
2016-01-17 16:55:06 -0500

    SDEditPrinter now finds classNames

--- src/problem/asm/SDEditPrinter.java
+++ src/problem/asm/SDEditPrinter.java
@@ -4,7 +4,9 @@ import java.io.FileOutputStream;
 import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SDEditPrinter implements IClassStructurePrinter {
 	
@@ -22,22 +24,30 @@ public class SDEditPrinter implements IClassStructurePrinter {
 		try {
 			StringBuilder sb = new StringBuilder();
 			OutputStream out = new FilterOutputStream(new FileOutputStream(file));
-			String nm = StringParser.parseClassName(this.classNames.get(0));
-			sb.append(nm + ":" + nm + "[a]\n");
 			
-			for (String name : this.classNames) {
-				name = StringParser.parseClassName(name);
-				if (!name.equals(nm) && !name.isEmpty()) {
-					sb.append("/" + name + ":" + name + "\n");
-				}
-			}
+			StringBuilder classesString = new StringBuilder();
+			Set<String> classSet = new HashSet<>();
+			String nm = StringParser.parseClassName(this.classNames.get(0));
+			classesString.append(nm + ":" + nm + "[a]\n");
+			
+//			for (String name : this.classNames) {
+//				name = StringParser.parseClassName(name);
+//				if (!name.equals(nm) && !name.isEmpty()) {
+//					sb.append("/" + name + ":" + name + "\n");
+//				}
+//			}
 			sb.append("\n");
 			
 			for (IMethodCallData meth : this.classes) {
+				if(!classSet.contains(meth.getMethodClass())){
+					classSet.add(meth.getMethodClass());
+					String name = StringParser.parseClassName(meth.getMethodClass());
+					classesString.append("/" + name + ":" + name + "\n");
+				}
 				sb.append(meth.toString() + "\n");
 			}
-			
-			out.write(sb.toString().getBytes());
+			classesString.append(sb.toString());
+			out.write(classesString.toString().getBytes());
 			out.close();
 		} catch (IOException exception) {
 			exception.printStackTrace();

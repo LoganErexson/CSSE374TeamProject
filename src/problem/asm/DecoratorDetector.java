0a2ced88c2a71f350b401b9f24c28072579a9cf3
Jack Porter <porterjc@rose-hulman.edu>
2016-02-03 22:15:46 -0500

    Decorator correctly detects decorators for 2-1 solution.

--- src/problem/asm/DecoratorDetector.java
+++ src/problem/asm/DecoratorDetector.java
@@ -7,6 +7,14 @@ public class DecoratorDetector implements IPatternDetector {
 
 	@Override
 	public void findPattern(IClassData d, IPackageModel m) {
+		IClassData sup = m.getClassDataFromName(d.getSuperClass());
+		if(sup == null)
+			return;
+		if (sup != null && !sup.hasPattern() && !d.hasPattern()) {
+			findPattern(sup, m);
+			if(d.hasPattern())
+				return;
+		}
 		for (IMethodData meth : d.getMethods()) {
 			if (meth.getName().equals("<init>")) {
 				for(String asc : d.getAssociatedClasses()) {
@@ -16,21 +24,22 @@ public class DecoratorDetector implements IPatternDetector {
 						if (in != null && !in.hasPattern()) {
 							in.setHasPattern(true);
 							in.setFill("fillcolor = green\n");
-							in.setPattern("\n\\<\\<component\\>\\>\n");
+							in.setPattern("\\l\\<\\<component\\>\\>\\l");
 						}
 						d.setHasPattern(true);
 						d.setFill("fillcolor = green\n");
-						d.setPattern("\n\\<\\<decorator\\>\\>\n");
+						d.setPattern("\\l\\<\\<decorator\\>\\>\\l");
+						return;
 					} 
 				}
 			}
 		}
-		IClassData sup = m.getClassDataFromName(d.getSuperClass());
 		if (sup != null && sup.hasPattern() && sup.getPattern().contains("decorator")) {
 			d.setHasPattern(true);
 			d.setFill("fillcolor = green\n");
-			d.setPattern("\n\\<\\<decorator\\>\\>\n");
-		}			
+			d.setPattern("\\l\\<\\<decorator\\>\\>\\l");
+			return;
+		}
 	}
 
 	@Override

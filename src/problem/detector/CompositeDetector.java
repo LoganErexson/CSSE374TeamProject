7a807d80635299a5fabe173a067ae793fab10762
Jack Porter <porterjc@rose-hulman.edu>
2016-02-08 21:37:34 -0500

    Added composite detector shell

---
+++ src/problem/detector/CompositeDetector.java
@@ -0,0 +1,20 @@
+package problem.detector;
+
+import problem.model.data.IClassData;
+import problem.model.data.IPackageModel;
+
+public class CompositeDetector implements IPatternDetector{
+
+	@Override
+	public void findPattern(IClassData d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void findPattern(IClassData d, IPackageModel m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

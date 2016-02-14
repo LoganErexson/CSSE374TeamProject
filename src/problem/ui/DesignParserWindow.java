699ce3054a400882827a9b40c72d6960591608f8
Logan Erexson <erexsolr@rose-hulman.edu>
2016-02-14 17:20:44 -0500

    Consolidated UI to one class for now

--- src/problem/ui/DesignParserWindow.java
+++ src/problem/ui/DesignParserWindow.java
@@ -1,5 +1,9 @@
 package problem.ui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -8,19 +12,55 @@ public class DesignParserWindow {
 	private JFrame frame;
 	private JPanel landingPanel;
 	private JPanel resultPanel;
+	private JButton loadButton;
+	private JButton analyzeButton;
+	private String configFile = "";
+	
 	public DesignParserWindow(){
 		this.frame = new JFrame("Design Parser");
-		this.landingPanel = new LandingPanel(this);
-		this.resultPanel = new ResultPanel();
+		this.buildLandingPanel();
+		this.buildResultPanel();
+		this.assignActions();
 		this.frame.add(this.landingPanel);
 	}
 	public void analyze(){
 		this.frame.remove(this.landingPanel);
 		this.frame.add(this.resultPanel);
 	}
+	private void buildLandingPanel(){
+		this.landingPanel = new JPanel();
+		this.loadButton = new JButton("Load Config");
+		this.analyzeButton = new JButton("Analyze");
+		this.landingPanel.add(this.loadButton);
+		this.landingPanel.add(this.analyzeButton);
+	}
+	private void buildResultPanel(){
+		this.resultPanel = new JPanel();
+	}
 	
+	private void assignActions() {
+		this.loadButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub.
+				
+			}
+			
+		});
+		this.analyzeButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub.
+				
+			}
+			
+		});
+	}
 	public void show() {
 		this.frame.setVisible(true);
 	}
+	
 
 }
--- src/problem/ui/ResultPanel.java
+++
@@ -1,9 +0,0 @@
-package problem.ui;
-
-import javax.swing.JPanel;
-
-public class ResultPanel extends JPanel{
-
-	private static final long serialVersionUID = 1L;
-	
-}

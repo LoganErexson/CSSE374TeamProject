5027a3f70c6550a03be1d2bcd22f04deeaace1ba
Logan Erexson <erexsolr@rose-hulman.edu>
2016-02-14 15:27:43 -0500

    Added beginnings of UI

---
+++ src/problem/ui/DesignParserWindow.java
@@ -0,0 +1,12 @@
+package problem.ui;
+
+import javax.swing.JFrame;
+
+public class DesignParserWindow {
+	
+	private JFrame frame;
+	public DesignParserWindow(){
+		this.frame = new JFrame("Design Parser");
+	}
+
+}
